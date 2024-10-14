9bd04bfbafdc2739597a90fc55ac0c9947760f8a
Kobidubidu <albiaja027@gmail.com>
2024-10-14 10:10:32 +0700

    dashboard untuk akun siswa

---
+++ pages/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+// Cek apakah pengguna sudah login dan apakah rolenya adalah user
+if (!isset($_SESSION['role']) || $_SESSION['role'] != 'user') {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Siswa - DKV SMKN 3</title>
+</head>
+<body>
+    <h1>Selamat datang, <?php echo $_SESSION['username']; ?> (Siswa)</h1>
+    <p>Ini adalah dashboard siswa.</p>
+    <a href="../pages/upload_portfolio.php">upload</a>
+    <a href="../actions/logout.php">Logout</a>
+</body>
+</html>
