bd563c409a80be3fca5b4988fd2720627dc5909c
Kobidubidu <albiaja027@gmail.com>
2024-10-14 10:13:37 +0700

    header allpage(blm selesai)

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeri Karya Siswa - DKV SMKN 3</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
