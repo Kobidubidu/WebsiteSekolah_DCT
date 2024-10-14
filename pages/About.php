1114864e734f7f8d3612959d4915560bd5c08c4a
Kobidubidu <albiaja027@gmail.com>
2024-10-14 10:11:43 +0700

    penyesuaian

--- pages/About.php
+++ pages/About.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="..\WebsiteSekolah_DCT\assets\css\About.css">
+    <link rel="stylesheet" href="..\assets\css\About.css">
     <style>
         .image-container {
             position: relative;
@@ -25,14 +25,14 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top" style="background-color: rgba(119, 48, 48, 0.5);">
         <div class="container">
-            <img src="..\WebsiteSekolah_DCT\assets\images\Logobrand.png" alt="Logo DKV SMKN 3 Bandung" width="100" height="100">
+            <img src="..\assets\images\Logobrand.png" alt="Logo DKV SMKN 3 Bandung" width="100" height="100">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="WebSekolah.html">Featured</a></li>
-                    <li class="nav-item"><a class="nav-link" href="..\WebsiteSekolah_DCT\pages\About.php">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="..\pages\About.php">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact_us.html">Contact Us</a></li>
                     <li class="nav-item"><a class="nav-link" href="EventDKV.html">Event DKV</a></li>
                     <li class="nav-item"><a class="nav-link" href="PortofolioSiswa.html">Portofolio Siswa</a></li>
