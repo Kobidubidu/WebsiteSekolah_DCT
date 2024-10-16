97153bc10531dbe62bca4fcd7195744fbff8380e
kobidubidu <albiaja027@gmail.com>
2024-10-16 20:32:52 +0700

    header all page

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeri Karya Siswa - DKV SMKN 3</title>
-    <!-- Bootstrap CSS -->
+    <title>About Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="..\assets\css\About.css">
 </head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top" style="background-color: rgba(119, 48, 48, 0.5);">
+        <div class="container">
+            <img src="..\assets\images\Logobrand.png" alt="Logo DKV SMKN 3 Bandung" width="100" height="100">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="../index.php">Featured</a></li>
+                    <li class="nav-item"><a class="nav-link" href="..\pages\About.php">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="eventdkv.php">Event DKV</a></li>
+                    <li class="nav-item"><a class="nav-link" href="portfolio.php">Portofolio Siswa</a></li>
+                </ul>
+                <a href="login.php" class="btn btn-primary ms-3">Login</a>
+            </div>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
