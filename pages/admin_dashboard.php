6cc0a9d050dbaec53281471545158ebcd6088d81
kobidubidu <albiaja027@gmail.com>
2024-10-19 15:02:47 +0700

    penambahan header

--- pages/admin_dashboard.php
+++ pages/admin_dashboard.php
@@ -16,6 +16,9 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
     <title>Dashboard Admin - DKV SMKN 3</title>
 </head>
 <body>
+    <?php
+    include '../includes/header.php';
+    ?>
     <h1>Selamat datang, <?php echo $_SESSION['username']; ?> (Admin)</h1>
     <p>Ini adalah dashboard admin.</p>
     <a href="../actions/logout.php">Logout</a>
